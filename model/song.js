692c306cff2e9ea3daba72dfa3eb0d921e6835c0
woodrow-snow <woodrow2021@outlook.com>
2026-06-06 13:47:13 -0600

    created and tested dev version of songs put route

--- model/song.js
+++ model/song.js
@@ -10,4 +10,39 @@ const songSchema = {
     playlists_in: [String]
 };
 
-module.exports = songSchema;
+// require statements
+const songModel = {};
+const mongo = require('../database/database');
+
+const getDB = () => {
+    return mongo.getDatabase().db().collection('song');
+};
+
+songModel.getSongById = async function (id) {
+    return await getDB().findOne({ _id: id});
+};
+
+songModel.updateSongById = async function (id, updatedSong) {
+    const ogSong = await this.getSongById(id);
+
+    // testing
+    console.log("OG:\n" + ogSong);
+
+    const results = await getDB().updateOne(
+        { _id: id },
+        {
+            $set: {
+                name: updatedSong.name ?? ogSong.name,
+                len_in_sec: updatedSong.len_in_sec ?? ogSong.len_in_sec,
+                len_in_min: updatedSong.len_in_min ?? ogSong.len_in_min,
+                artist_id: updatedSong.artist_id ?? ogSong.artist_id,
+                album_id: updatedSong.album_id ?? ogSong.album_id,
+                playlists_in: updatedSong.playlists_i ?? ogSong.playlists_in
+            }
+        }
+    );
+
+    return results;
+};
+
+module.exports = { songSchema, songModel};
