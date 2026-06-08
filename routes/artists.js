9aa4181b077483cad1598f8054daa04712958a5d
woodrow-snow <woodrow2021@outlook.com>
2026-06-07 21:47:41 -0600

    updated swagger routes information

--- routes/artists.js
+++ routes/artists.js
@@ -1,4 +1,51 @@
 /* ***********************************************
+ * Artist Schema and other fun stuff
+ * *********************************************** */
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Artist:
+ *       type: object
+ *       required:
+ *         - name
+ *         - songs
+ *         - album
+ *         - listeners
+ *       properties:
+ *         _id:
+ *           type: string
+ *           description: Unique MongoDB ObjectId for the artist
+ *           example: "661f1a2b3c4d5e6f7a8b9c10"
+ *         name:
+ *           type: string
+ *           description: Name of the artist or band
+ *           example: Queen
+ *         songs:
+ *           type: array
+ *           description: Array of song ObjectIds associated with the artist
+ *           items:
+ *             type: string
+ *             example: "661f1a2b3c4d5e6f7a8b9c01"
+ *         album:
+ *           type: array
+ *           description: Array of album ObjectIds associated with the artist
+ *           items:
+ *             type: string
+ *             example: "661f1a2b3c4d5e6f7a8b9c20"
+ *         listeners:
+ *           type: integer
+ *           description: Number of monthly listeners
+ *           example: 52000000
+ */
+/**
+ * @swagger
+ * tags:
+ *   name: Artists
+ *   description: The Artist managing API
+ */
+
+/* ***********************************************
  * Artist Routes
  * *********************************************** */
 const express = require('express');
@@ -11,6 +58,7 @@ const artistsController = require('../controllers/artists');
  * /artist:
  *   get:
  *     summary: Get all artists
+ *     tags: [Artists]
  *     responses:
  *       200:
  *         description: Success
@@ -22,6 +70,7 @@ router.get('/', artistsController.getAllArtists);
  * /artist/{id}:
  *   get:
  *     summary: Get artist by id
+ *     tags: [Artists]
  *     parameters:
  *       - in: path
  *         name: id
@@ -34,10 +83,66 @@ router.get('/', artistsController.getAllArtists);
  */
 router.get('/:id', artistsController.getArtistById);
 
+/**
+ * @swagger
+ * /artist:
+ *   post:
+ *     summary: Create a new Artist
+ *     tags: [Artists]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *          application/json: 
+ *              schema:
+ *                  $ref: '#/components/schemas/Artist'
+ *     responses:
+ *       200:
+ *         description: Success
+ *       500:
+ *         description: Artist not created
+ */
 router.post('/', artistsController.createArtist);
 
+/**
+ * @swagger
+ * /artist/{id}:
+ *   put:
+ *     summary: Update an Artist
+ *     tags: [Artists]
+ *     parameters:
+ *      - in: path
+ *        name: id
+ *        required: true
+ *     requestBody:
+ *       required: true
+ *       content:
+ *          application/json: 
+ *              schema:
+ *                  $ref: '#/components/schemas/Artist'
+ *     responses:
+ *       200:
+ *         description: Success
+ *       500:
+ *         description: Artist not updated
+ */
 router.put('/:id', artistsController.updateArtistById);
 
+/**
+ * @swagger
+ * /artist/{id}:
+ *   delete:
+ *     summary: Delete an artist
+ *     tags: [Artists]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *     responses:
+ *       200:
+ *          description: Artist was deleted
+ *       500:
+ *          description: An error occurred while attempting to delete the artist
+ */
 router.delete('/:id', artistsController.deleteArtistById);
 
 module.exports = router;
--- routes/songs.js
+++ routes/songs.js
@@ -1,4 +1,64 @@
 /* ***********************************************
+ * Song Schema
+ * *********************************************** */
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Song:
+ *       type: object
+ *       required:
+ *         - name
+ *         - len_in_sec
+ *         - len_in_min
+ *         - artist_id
+ *         - album_id
+ *       properties:
+ *         _id:
+ *           type: string
+ *           description: Unique MongoDB ObjectId for the song
+ *           example: "661f1a2b3c4d5e6f7a8b9c01"
+ *         
+ *         name:
+ *           type: string
+ *           description: Name of the song
+ *           example: "Bohemian Rhapsody"
+ *         
+ *         len_in_sec:
+ *           type: integer
+ *           description: Song length in seconds
+ *           example: 354
+ *         
+ *         len_in_min:
+ *           type: integer
+ *           description: Song length in minutes
+ *           example: 6
+ *         
+ *         artist_id:
+ *           type: string
+ *           description: MongoDB ObjectId reference to the artist
+ *           example: "661f1a2b3c4d5e6f7a8b9c10"
+ *         
+ *         album_id:
+ *           type: string
+ *           description: MongoDB ObjectId reference to the album
+ *           example: "661f1a2b3c4d5e6f7a8b9c20"
+ *         
+ *         playlists_in:
+ *           type: array
+ *           description: Array of playlist ObjectIds containing this song
+ *           items:
+ *             type: string
+ *             example: "661f1a2b3c4d5e6f7a8b9c30"
+ */
+/**
+ * @swagger
+ * tags:
+ *      name: Songs
+ *      description: The Song managing API
+ */
+
+/* ***********************************************
  * Song Routes
  * *********************************************** */
 const express = require('express');
@@ -11,6 +71,7 @@ const songsController = require('../controllers/songs');
  * /song:
  *   get:
  *     summary: Get all songs
+ *     tags: [Songs]
  *     responses:
  *       200:
  *         description: Success
@@ -22,6 +83,7 @@ router.get('/', songsController.getAllSongs);
  * /song/{id}:
  *   get:
  *     summary: Get song by id
+ *     tags: [Songs]
  *     parameters:
  *       - in: path
  *         name: id
@@ -34,10 +96,66 @@ router.get('/', songsController.getAllSongs);
  */
 router.get('/:id', songsController.getSongById);
 
+/**
+ * @swagger
+ * /song:
+ *   post:
+ *     summary: Create a new Song
+ *     tags: [Songs]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *          application/json: 
+ *              schema:
+ *                  $ref: '#/components/schemas/Song'
+ *     responses:
+ *       200:
+ *         description: Success
+ *       500:
+ *         description: Song not created
+ */
 router.post('/', songsController.createSong);
 
+/**
+ * @swagger
+ * /song/{id}:
+ *   put:
+ *     summary: Update a song
+ *     tags: [Songs]
+ *     parameters:
+ *      - in: path
+ *        name: id
+ *        required: true
+ *     requestBody:
+ *       required: true
+ *       content:
+ *          application/json: 
+ *              schema:
+ *                  $ref: '#/components/schemas/Song'
+ *     responses:
+ *       200:
+ *         description: Success
+ *       500:
+ *         description: Song not updated
+ */
 router.put('/:id', songsController.updateSongById);
 
+/**
+ * @swagger
+ * /song/{id}:
+ *   delete:
+ *     summary: Delete a song
+ *     tags: [Songs]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *     responses:
+ *       200:
+ *          description: Song was deleted
+ *       500:
+ *          description: An error occurred while attempting to delete the song
+ */
 router.delete('/:id', songsController.deleteSongById);
 
 module.exports = router;
